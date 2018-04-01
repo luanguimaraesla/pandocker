99be849fb42038f82fee9ea3581163639ad1e03a
Luan Guimarães <luang@riseup.net>
2018-04-01 00:27:28 -0300

    Improve Pandocker module documentation

--- lib/pandocker.ex
+++ lib/pandocker.ex
@@ -1,8 +1,24 @@
 defmodule Pandocker do
   @moduledoc """
-  Documentation for Pandocker.
+  This module defines some Pandocker tasks
+
+  Functions:
+
+    - `run/0`: runs pandoc by concatenating the list of files described in
+      the section 'sections' of the YAML configuration file.
   """
 
+  @doc """
+  runs pandoc by concatenating the list of files described in the section
+  'sections' of the YAML configuration file
+
+  ## Example:
+
+    iex> System.put_env("PANDOCKER_CONFIG_YAML", "test.yml")
+    iex> Pandocker.run
+    :ok
+
+  """
   def run do
     ConfigManager.get_yaml_section(:sections)
     |> Pandoc.compile
